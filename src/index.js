13e8bef5554922828e917a15c2c23de6907ed1d1
Igor-Pino <igorpinyo@gmail.com>
2021-09-23 18:33:02 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import './sass/main.scss';
+import debounce from 'lodash.debounce';
+import ApiService from './partials/js-partials/api-service';
+import imageCardTpl from './templates/image-card_templates.hbs';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    imageContainer: document.querySelector('.gallery-js'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+}
+
+
+const apiService = new ApiService()
+
+refs.searchForm.addEventListener('input', debounce(onInputChange, 1000))
+refs.loadMoreBtn.addEventListener('click', onLoadMore)
+
+
+
+
+
+function onInputChange(e) {
+    clearImageContainer()
+    apiService.query = e.target.value;
+    if(apiService.query === '') {return alert('input something')}
+    apiService.resetPage()
+
+    apiService.fetchImageByTag().then(appendImageMarkup);
+
+
+}
+
+
+function onLoadMore() {
+  
+    apiService.fetchImageByTag().then(appendImageMarkup)
+}
+
+function appendImageMarkup (data) {
+    const markup = imageCardTpl(data);
+    refs.imageContainer.insertAdjacentHTML('beforeend', markup)
+    console.log(data)
+
+
+}
+
+
+function clearImageContainer () {
+    refs.imageContainer.innerHTML = '';
+}
---
+++ src/partials/js-partials/api-service.js
@@ -0,0 +1,39 @@
+console.log('hi')
+
+export default class ApiService {
+constructor() {
+    this.searchQuery = '';
+    this.API_KEY = '23521074-c1847750f84d7ba2d97c15f75';
+    this.BASE_URL = 'https://pixabay.com/api/';
+    this.page = 1;
+
+}
+
+    async fetchImageByTag() {
+        const url = `${this.BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.API_KEY}`
+        const response = await fetch(url);
+        const data = await response.json();
+        this.incrementPage();
+        return data.hits;
+    
+  
+    }
+
+    incrementPage (){
+        this.page += 1;
+    }
+
+    resetPage () {
+        this.page = 1;
+    }
+
+    get query () {
+        return this.searchQuery
+    }
+
+    set query (newQuery){
+        this.searchQuery = newQuery
+    }
+
+
+}
